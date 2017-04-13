e6bda22306c643cc326d172ddd09d0ec7cc404fb
lixiaojian <mistlxj@163.com>
2017-04-13 14:36:17 +0800

    【add】cglib 进一步研究

--- mistCode/src/main/java/thinkingJava/dynamicProxy/cglib/Base.java
+++ mistCode/src/main/java/thinkingJava/dynamicProxy/cglib/Base.java
@@ -5,7 +5,22 @@ package thinkingJava.dynamicProxy.cglib;
  * 目标类
  */
 public class Base {
+
+    private String name;
+
+    public Base(String name) {
+        this.name = name;
+    }
+
+    public Base() {}
+
+    //新增，只有授权才能做写操作
     public void add() {
-        System.out.println("base added");
+        System.out.println("invoke base add()");
+    }
+
+    //查询 query 方法不做权限控制，所有人都可以访问
+    public void query() {
+        System.out.println("invoke base query()");
     }
 }
--- mistCode/src/main/java/thinkingJava/dynamicProxy/cglib/Test.java
+++ mistCode/src/main/java/thinkingJava/dynamicProxy/cglib/Test.java
@@ -4,11 +4,23 @@ package thinkingJava.dynamicProxy.cglib;
  * Created by lixiaojian on 2017/4/12.
  */
 public class Test {
-    // http://blog.csdn.net/dreamrealised/article/details/12885739
+
     public static void main(String[] args) {
-        CglibProxy proxy = new CglibProxy();
         // base为生成的增强过的目标类
-        Base base = Factory.getInstance(proxy);
-        base.add();
+        Base base1 = Factory.getProxyInstance(new MyCglibProxy(true));
+        base1.add();
+        base1.query();
+        System.out.println("-------------------------------------------");
+//        Base base2 = Factory.getProxyInstance(new MyCglibProxy(false));
+//        base2.add();
+//        base2.query();
+//        System.out.println("-------------------------------------------");
+//        Base base3 = Factory.getProxyInstanceByFilter(new MyCglibProxy(true));
+//        base3.add();
+//        base3.query();
+//        System.out.println("-------------------------------------------");
+//        Base base4 = Factory.getProxyInstanceByFilter(new MyCglibProxy(false));
+//        base4.add();
+//        base4.query();
     }
 }
