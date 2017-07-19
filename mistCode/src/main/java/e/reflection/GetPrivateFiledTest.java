9550c8408eaa35a3261607b34e91c419542e6d54
lixiaojian <mistlxj@163.com>
2017-07-19 18:54:34 +0800

    【add】反射获取类的私有属性

---
+++ mistCode/src/main/java/e/reflection/GetPrivateFiledTest.java
@@ -0,0 +1,22 @@
+package e.reflection;
+
+import java.lang.reflect.Field;
+import java.lang.reflect.Modifier;
+
+/**
+ * Created by lixiaojian on 2017/7/19.
+ */
+public class GetPrivateFiledTest {
+
+    public static void main(String[] args) {
+//        Field tempApiMap = ApiService.class.getDeclaredField("tempApiMap");
+//        if (Modifier.isStatic(tempApiMap.getModifiers())) {
+//            if (!tempApiMap.isAccessible()) {
+//                tempApiMap.setAccessible(true);
+//            }
+//            LRUMap<String, ApiModel> tempMap = (LRUMap<String, ApiModel>) tempApiMap.get(ApiService.class);
+//            return tempMap;
+//        }
+    }
+
+}
