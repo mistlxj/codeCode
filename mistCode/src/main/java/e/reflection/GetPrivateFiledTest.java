3d4323e2ec31ab90cd25b3792bbf6dcb9fd43ca9
lixiaojian <mistlxj@163.com>
2017-07-20 16:12:54 +0800

    【add】反射获取类的私有属性

--- mistCode/src/main/java/e/reflection/GetPrivateFiledTest.java
+++ mistCode/src/main/java/e/reflection/GetPrivateFiledTest.java
@@ -2,21 +2,47 @@ package e.reflection;
 
 import java.lang.reflect.Field;
 import java.lang.reflect.Modifier;
+import java.util.HashMap;
+import java.util.Map;
+
 
 /**
  * Created by lixiaojian on 2017/7/19.
  */
 public class GetPrivateFiledTest {
 
-    public static void main(String[] args) {
-//        Field tempApiMap = ApiService.class.getDeclaredField("tempApiMap");
-//        if (Modifier.isStatic(tempApiMap.getModifiers())) {
-//            if (!tempApiMap.isAccessible()) {
-//                tempApiMap.setAccessible(true);
-//            }
-//            LRUMap<String, ApiModel> tempMap = (LRUMap<String, ApiModel>) tempApiMap.get(ApiService.class);
-//            return tempMap;
-//        }
+    public static void main(String[] args) throws NoSuchFieldException, IllegalAccessException, InstantiationException {
+        //
+        Object res = getFieldsByReflection(PrimitiveClass.class, "tempMap");
+        if (res instanceof Map) {
+            res = (HashMap<String,Object>) res;
+            for (Map.Entry e : ((HashMap<String, Object>) res).entrySet()) {
+                System.out.println(e.getKey() + " = " + e.getValue());
+            }
+        }
+        getFieldsByReflection(PrimitiveClass.class);
+
+    }
+
+    private static void getFieldsByReflection(Class<?> clazz) throws IllegalAccessException, InstantiationException {
+        Field[] fields = clazz.getDeclaredFields();
+        for (Field f : fields) {
+            if (!f.isAccessible()) {
+                f.setAccessible(true);
+                System.out.println(f.getName() + " = " + f.get(clazz.newInstance()));
+            }
+        }
+    }
+
+    private static Object getFieldsByReflection(Class<?> clazz, String filedName) throws NoSuchFieldException,
+            IllegalAccessException {
+        Field field = clazz.getDeclaredField(filedName);
+        System.out.println(Modifier.isStatic(field.getModifiers()));
+        if (!field.isAccessible()) {
+            field.setAccessible(true);
+        }
+        Object res = field.get(clazz);
+        return res;
     }
 
 }
