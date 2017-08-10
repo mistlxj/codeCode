bce5a5500a011b81b4b7ffba83c226cf52d31cbe
lixiaojian <mistlxj@163.com>
2017-08-10 15:24:50 +0800

    【add】利用ParameterizedType获取泛型化类型的参数

---
+++ mistCode/src/main/java/thinkingJava/generic/patameterizedType/TestDemo.java
@@ -0,0 +1,30 @@
+package thinkingJava.generic.patameterizedType;
+
+import com.zuche.framework.common.SpringApplicationContext;
+
+import java.lang.reflect.ParameterizedType;
+import java.lang.reflect.Type;
+import java.util.Map;
+
+/**
+ * Created by lixiaojian on 2017/8/10.
+ * @des 利用ParameterizedType获取泛型化类型的参数
+ */
+public class TestDemo {
+    public static void main(String[] args) {
+        Class paramClass = Object.class;
+        Type[] types = Base.class.getGenericInterfaces();
+        for (Type t : types) {
+            if (t instanceof ParameterizedType) {
+                ParameterizedType type = (ParameterizedType) t;
+                if (type.getRawType() == BaseInterface.class) {
+                    Type[] paramsType = type.getActualTypeArguments();
+                    if (null != paramsType && paramsType.length > 1 && paramsType[1] instanceof Class) {
+                        paramClass = (Class) paramsType[1];
+                    }
+                }
+            }
+        }
+        System.out.println(paramClass.getName());
+    }
+}
